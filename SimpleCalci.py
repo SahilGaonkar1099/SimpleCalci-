b4b0c30469a434f73b00bc24b73a12b48ced4356
Sahil P Gaonkar <60326413+SahilGaonkar1099@users.noreply.github.com>
2020-06-09 18:45:21 +0530

    Update SimpleCalci.py

--- SimpleCalci.py
+++ SimpleCalci.py
@@ -9,7 +9,7 @@ def equalpress():
 	try:
 
 		global expression
-		total = str(eval(expression))
+		total = str(eval(expression))#Evaluates expression directly so that we don't need to write different or separate functions for evaluation
 
 		equation.set(total)
 
@@ -17,7 +17,7 @@ def equalpress():
 
 	except:
 
-		equation.set(" error ")
+		equation.set(" ERROR ")
 		expression = ""
 
 def clear():
@@ -29,93 +29,93 @@ def clear():
 # Driver code
 if __name__ == "__main__":
 	# create a GUI window
-	gui = Tk()
+	PPB = Tk()
 
 	# set the background colour of GUI window
-	gui.configure(background="black")
+	PPB.configure(background="black")
 
 	# set the title of GUI window
-	gui.title("Simple Calculator")
+	PPB.title("Simple Calculator")
 
 	# set the configuration of GUI window
-	gui.geometry("350x250")
+	PPB.geometry("350x250")
 
 
 	equation = StringVar()
 
 
-	expression_field = Entry(gui, textvariable=equation)
+	expression_field = Entry(PPB, textvariable=equation)
 
 	expression_field.grid(columnspan=4, ipadx=70)
 
-	equation.set('enter your expression')
+	equation.set('Enter')
 
-	button1 = Button(gui, text=' 1 ', fg='black', bg='red',
+	button1 = Button(PPB, text=' 1 ', fg='black', bg='red',
 					command=lambda: press(1), height=1, width=7)
 	button1.grid(row=2, column=0)
 
-	button2 = Button(gui, text=' 2 ', fg='black', bg='red',
+	button2 = Button(PPB, text=' 2 ', fg='black', bg='red',
 					command=lambda: press(2), height=1, width=7)
 	button2.grid(row=2, column=1)
 
-	button3 = Button(gui, text=' 3 ', fg='black', bg='red',
+	button3 = Button(PPB, text=' 3 ', fg='black', bg='red',
 					command=lambda: press(3), height=1, width=7)
 	button3.grid(row=2, column=2)
 
-	button4 = Button(gui, text=' 4 ', fg='black', bg='red',
+	button4 = Button(PPB, text=' 4 ', fg='black', bg='red',
 					command=lambda: press(4), height=1, width=7)
 	button4.grid(row=3, column=0)
 
-	button5 = Button(gui, text=' 5 ', fg='black', bg='red',
+	button5 = Button(PPB, text=' 5 ', fg='black', bg='red',
 					command=lambda: press(5), height=1, width=7)
 	button5.grid(row=3, column=1)
 
-	button6 = Button(gui, text=' 6 ', fg='black', bg='red',
+	button6 = Button(PPB, text=' 6 ', fg='black', bg='red',
 					command=lambda: press(6), height=1, width=7)
 	button6.grid(row=3, column=2)
 
-	button7 = Button(gui, text=' 7 ', fg='black', bg='red',
+	button7 = Button(PPB, text=' 7 ', fg='black', bg='red',
 					command=lambda: press(7), height=1, width=7)
 	button7.grid(row=4, column=0)
 
-	button8 = Button(gui, text=' 8 ', fg='black', bg='red',
+	button8 = Button(PPB, text=' 8 ', fg='black', bg='red',
 					command=lambda: press(8), height=1, width=7)
 	button8.grid(row=4, column=1)
 
-	button9 = Button(gui, text=' 9 ', fg='black', bg='red',
+	button9 = Button(PPB, text=' 9 ', fg='black', bg='red',
 					command=lambda: press(9), height=1, width=7)
 	button9.grid(row=4, column=2)
 
-	button0 = Button(gui, text=' 0 ', fg='black', bg='red',
+	button0 = Button(PPB, text=' 0 ', fg='black', bg='red',
 					command=lambda: press(0), height=1, width=7)
 	button0.grid(row=5, column=0)
 
-	plus = Button(gui, text=' + ', fg='black', bg='red',
+	plus = Button(PPB, text=' + ', fg='black', bg='red',
 				command=lambda: press("+"), height=1, width=7)
 	plus.grid(row=2, column=3)
 
-	minus = Button(gui, text=' - ', fg='black', bg='red',
+	minus = Button(PPB, text=' - ', fg='black', bg='red',
 				command=lambda: press("-"), height=1, width=7)
 	minus.grid(row=3, column=3)
 
-	multiply = Button(gui, text=' * ', fg='black', bg='red',
+	multiply = Button(PPB, text=' * ', fg='black', bg='red',
 					command=lambda: press("*"), height=1, width=7)
 	multiply.grid(row=4, column=3)
 
-	divide = Button(gui, text=' / ', fg='black', bg='red',
+	divide = Button(PPB, text=' / ', fg='black', bg='red',
 					command=lambda: press("/"), height=1, width=7)
 	divide.grid(row=5, column=3)
 
-	equal = Button(gui, text=' = ', fg='black', bg='red',
+	equal = Button(PPB, text=' = ', fg='black', bg='red',
 				command=equalpress, height=1, width=7)
 	equal.grid(row=5, column=2)
 
-	clear = Button(gui, text='Clear', fg='black', bg='red',
+	Decimal = Button(PPB, text='.', fg='black', bg='red',
+					 command=lambda: press('.'), height=1, width=7)
+	Decimal.grid(row=5, column=1)
+	clear = Button(PPB, text='Clear', fg='black', bg='red',
 				command=clear, height=1, width=7)
-	clear.grid(row=5, column='1')
+	clear.grid(row=6, column='0')
 
-	Decimal= Button(gui, text='.', fg='black', bg='red',
-					command=lambda: press('.'), height=1, width=7)
-	Decimal.grid(row=6, column=0)
-	# start the GUI
-	gui.mainloop()
+
+	PPB.mainloop()
